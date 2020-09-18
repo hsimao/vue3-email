c6d61d784516e7b87cdc7980b2181c9b38b20650
hsimao <e087754958@gmail.com>
2020-09-18 12:13:09 +0800

    add: use json-server

--- src/components/MailTable.vue
+++ src/components/MailTable.vue
@@ -4,7 +4,7 @@
       <tr v-for="email in unarchivedEmails"
         :key="email.id"
         :class="['clickable', email.read ? 'read' : '']"
-        @click="email.read = true">
+        @click="readEmail(email)">
         <td><input type="checkbox" /></td>
         <td>{{ email.from }}</td>
         <td>
@@ -17,7 +17,7 @@
         </td>
         <td>
           <button
-            @click="email.archived = true">
+            @click="archiveEmail(email)">
             Archive
           </button>
         </td>
@@ -28,56 +28,16 @@
 
 <script>
 import { format, getTime } from 'date-fns';
+import axios from 'axios';
 import { ref, computed } from 'vue';
+
+const baseUrl = 'http://localhost:3000';
 
 export default {
   name: 'MailTable',
   async setup() {
-    await new Promise(resolve => setTimeout(resolve, 3000));
-
-    const emails = ref([
-      {
-        id: 1,
-        from: 'team@vuemastery.com',
-        subject: "What's up with Vue 3.0? Here's how to find out from Evan You",
-        body:
-          "The opening keynote of VueConf US this year was Evan You (the creator of Vue), giving his State of the Vuenion address. He walked us through the journey of getting Vue 3 from a prototype to a reality the past year. He also dove into Vue's overall growth in the community.",
-        sentAt: '2020-03-27T18:25:43.511Z',
-        archived: false,
-        read: true
-      },
-      {
-        id: 2,
-        from: 'jeffrey@vuetraining.net',
-        subject: 'Learn by doing - Vue 3 Zero to Intermediate in 8 weeks',
-        body:
-          "Building projects is one of the most effective ways to learn - and _the_ most effective way _remember_ what you've learned - but it can be frustrating.\n\nThis 8-week course takes the pain out of 'learning by doing'.\n\nEach week we give you\n\n* a project that will grow your skills without overwhelming you\n* links to hand-picked resources, such as Vue Mastery videos, that share the knowledge you'll need for the project (no more useless rabbit holes)\n* answers to any and all questions you have while working\n* feedback on your completed code (so you're only learning good habits)\n\nOur instructors are standing by to answer your questions.\n\nReady to learn?",
-        sentAt: '2020-05-20T18:25:43.511Z',
-        archived: false,
-        read: false
-      },
-      {
-        id: 3,
-        from: 'damian@dulisz.com',
-        subject:
-          '#177: Updated Vue.js Roadmap; Vuex v4.0.0-alpha.1 has been released; Kia King Ishii join the core team; Nuxt v2.12 released; Videos from Vue.js Amsterdam 2020 are here!',
-        body:
-          'First of all, lets congratulate Kia King Ishii on joining the Vue.js core team! 🎉 He has been doing an incredible job building vuex-orm and will now focus on working on the next versions of Vuex.\n\nSpeaking of which – Vuex v4.0.0-alpha.1 has just been released! This is the version of Vuex that will work with Vue 3.0 but keep the familiar API you know from the current version.',
-        sentAt: '2020-03-18T18:25:43.511Z',
-        archived: false,
-        read: false
-      },
-      {
-        id: 4,
-        from: 'anthony@vuejsdevelopers.com',
-        subject:
-          "'Vue 3 Release Roadmap' + 6 more must-read articles from this week",
-        body: 'Newsletter Issue #161',
-        sentAt: '2020-03-24T18:25:43.511Z',
-        archived: true,
-        read: false
-      }
-    ]);
+    const { data } = await axios.get(`${baseUrl}/emails`);
+    const emails = ref(data);
 
     const sortedEmails = computed(() => {
       return emails.value.sort((a, b) => {
@@ -89,10 +49,26 @@ export default {
       return sortedEmails.value.filter(email => !email.archived);
     });
 
+    const updateEmail = email => {
+      axios.put(`${baseUrl}/emails/${email.id}`, email);
+    };
+
+    const readEmail = email => {
+      email.read = true;
+      updateEmail(email);
+    };
+
+    const archiveEmail = email => {
+      email.archived = true;
+      updateEmail(email);
+    };
+
     return {
       format,
       getTime,
-      unarchivedEmails
+      unarchivedEmails,
+      readEmail,
+      archiveEmail
     };
   }
 };
